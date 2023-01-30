bf5bf65c8ede15b1318ceb50f431bfa48259222c
Rahul <malawaliyarahul@gmail.com>
2023-01-30 16:58:20 +0530

    code

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -1,17 +1,21 @@
 package com.driver;
 
+import io.swagger.models.auth.In;
+import org.springframework.stereotype.Repository;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.stereotype.Repository;
-
 @Repository
 public class WhatsappRepository {
-	
-	Map<User,String> usermap=new HashMap<>();
-	Map<Group,List<User>> groupMap=new HashMap<>();
-	Map<Group,List<Message>> messageMap=new HashMap<>();
-	
-
+    public Map<String ,User> userMap= new HashMap<>();
+    public Map<Group,List<User>> personalGroup = new HashMap<>();
+    public Map<Group,List<User>> publicGroup = new HashMap<>();
+    public Map<User,Group> userGroupMap = new HashMap<>();
+    public Map<User,Group> admins = new HashMap<>();
+    public Map<Group,List<User>> allGroups = new HashMap<>();
+    public Map<Integer,Message> messageMap = new HashMap<>();
+    public Map<User,List<Message>> userMessages = new HashMap<>();
+    public Map<Group,List<Message>> groupMessages = new HashMap<>();
 }
--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -1,115 +1,115 @@
 package com.driver;
+import org.springframework.stereotype.Service;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import org.springframework.stereotype.Service;
+import java.util.Map;
 
 @Service
 public class WhatsappService {
-	
-	WhatsappRepository repository=new WhatsappRepository();
+    WhatsappRepository whatsappRepository = new WhatsappRepository();
+    public String createUser(String name, String mobile) throws Exception {
+        if(whatsappRepository.userMap.containsKey(mobile))throw new Exception("User already exists");
+        else{
+            User user = new User();
+            user.setMobile(mobile);
+            user.setName(name);
+            whatsappRepository.userMap.put(mobile,user);
+            return "SUCCESS";
+        }
+    }
+    public Group createGroup(List<User> users) {
+        Group group = new Group();
+        if(users.size()>1){
+            if(users.size()==2){
+                group.setName(users.get(1).getName());
+                group.setNumberOfParticipants(2);
+                whatsappRepository.personalGroup.put(group,users);
 
-	public String createUser(String name, String mobile) throws Exception {
-		// TODO Auto-generated method stub
-		if(repository.usermap.containsValue(mobile))
-		{
-			throw new Exception("User already exists");
-		}
-		User user=new User();
-		user.setName(name);
-		user.setMobile(mobile);
-		
-		repository.usermap.put(user, mobile);
-		
-		return "SUCCESS";
-	}
+            }
+            if(users.size()>2){
+                int count = whatsappRepository.publicGroup.size()+1;
+                group.setName("Group "+ count);
+                group.setNumberOfParticipants(users.size());
+                whatsappRepository.publicGroup.put(group,users);
+            }
+            for(int i=0;i<users.size();i++){
+                whatsappRepository.userGroupMap.put(users.get(i),group);
+            }
+            whatsappRepository.admins.put(users.get(0),group);
+            whatsappRepository.allGroups.put(group,users);
+        }
+        return group;
+    }
+    public int createMessage(String content) {
+        Message message = new Message();
+        message.setContent(content);
+        message.setId(whatsappRepository.messageMap.size()+1);
+        whatsappRepository.messageMap.put(whatsappRepository.messageMap.size()+1,message);
+        return message.getId();
+    }
+    public int sendMessage(Message message, User sender, Group group) throws Exception{
+        if(whatsappRepository.allGroups.containsKey(group)){
+            if(whatsappRepository.userGroupMap.containsKey(sender) && whatsappRepository.userGroupMap.get(sender)==group){
+                List<Message> usersMessageList = whatsappRepository.userMessages.getOrDefault(sender,new ArrayList<>());
+                usersMessageList.add(message);
+                whatsappRepository.userMessages.put(sender,usersMessageList);
+                List<Message> groupsMessageList = whatsappRepository.groupMessages.getOrDefault(group,new ArrayList<>());
+                groupsMessageList.add(message);
+                whatsappRepository.groupMessages.put(group,groupsMessageList);
+                return groupsMessageList.size();
+            }
+            else throw new Exception("You are not allowed to send message");
+        }
+        else throw new Exception("Group does not exist");
+    }
+    public String changeAdmin(User approver, User user, Group group) throws Exception {
+        if(whatsappRepository.allGroups.containsKey(group)){
+            if(whatsappRepository.admins.get(approver)==group){
+                if(whatsappRepository.allGroups.get(group).indexOf(user)<1)throw new Exception("User is not a participant");
+                else{
+                    whatsappRepository.admins.remove(approver);
+                    whatsappRepository.admins.put(user,group);
+                    return "SUCCESS";
+                }
 
-	public Group createGroup(List<User> users) {
-		// TODO Auto-generated method stub
-		Group group=new Group();
-		String s="Group ";
-		int size=users.size();
-		if(size<=2)
-		{
-			group.setAdmin(users.get(0));
-			group.setNumberOfParticipants(size);
-			group.setName(users.get(size-1).getName());
-		}
-		else
-		{
-			group.setAdmin(users.get(0));
-			group.setNumberOfParticipants(size);
-			group.setName(s+repository.groupMap.size()+1);
-		}
-		repository.groupMap.put(group, users);
-		return group;
-	}
-
-	public int createMessage(String content) {
-		// TODO Auto-generated method stub
-		
-		Message message=new Message();
-		message.setContent(content);
-		message.setId(repository.messageMap.size()+1);
-		List<Message> messages=new ArrayList<>();
-		messages.add(message);
-		repository.messageMap.put(null, messages);
-		
-		return repository.messageMap.size()+1;
-	}
-
-	public int sendMessage(Message message, User sender, Group group) throws Exception {
-		// TODO Auto-generated method stub
-		if(!repository.groupMap.containsKey(group))
-		{
-			throw new Exception("Group does not exist");
-		}
-		List<User> users=repository.groupMap.get(group);
-		if(!users.contains(sender))
-		{
-			throw new Exception("You are not allowed to send message");
-		}
-		List<Message> messages=repository.messageMap.get(group);
-		messages.add(message);
-		repository.messageMap.put(group, messages);
-		
-		return repository.messageMap.size()+1;
-	}
-
-	public String changeAdmin(User approver, User user, Group group) throws Exception {
-		// TODO Auto-generated method stub
-		if(!repository.groupMap.containsKey(group))
-		{
-			throw new Exception("Group does not exist");
-		}
-		List<User> users=repository.groupMap.get(group);
-		if(!users.contains(approver))
-		{
-			throw new Exception("Approver does not have rights");
-		}
-		if(!users.contains(user))
-		{
-			throw new Exception("User is not a participant");
-		}
-		Group group2=group;
-		group2.setAdmin(user);
-		repository.groupMap.remove(group);
-		repository.groupMap.put(group2, users);
-		
-		return "SUCCESS";
-	}
-
-	public int removeUser(User user) {
-		// TODO Auto-generated method stub
-		
-		return 0;
-	}
-
-	public String findMessage(Date start, Date end, int k) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+            }
+            else {
+                throw new Exception("Approver does not have rights");
+            }
+        }
+        else{
+            throw new Exception("Group does not exist");
+        }
+    }
+    public int removeUser(User user) throws Exception {
+        if(whatsappRepository.userGroupMap.containsKey(user)){
+            if(!whatsappRepository.admins.containsKey(user)){
+                List<Message> usersMessageList = whatsappRepository.userMessages.get(user);
+                whatsappRepository.userMessages.remove(user);
+                Group group = whatsappRepository.userGroupMap.get(user);
+                List<Message> groupsMessageList = whatsappRepository.groupMessages.get(group);
+                for(Message message:usersMessageList){
+                    groupsMessageList.remove(message);
+                    whatsappRepository.messageMap.remove(message.getId());
+                }
+                whatsappRepository.groupMessages.put(group,groupsMessageList);
+                List<User> userList = whatsappRepository.allGroups.get(group);
+                userList.remove(user);
+                whatsappRepository.allGroups.put(group,userList);
+                return groupsMessageList.size()+userList.size()+whatsappRepository.messageMap.size();
+            }
+            else{
+                throw new Exception("Cannot remove admin");
+            }
+        }
+        else {
+             throw new Exception("User not found");
+        }
+    }
+    public String findMessage(Date start, Date end, int k) {
+        return "";
+    }
 }
